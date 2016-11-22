ac7f322aa9d39d22a50073bcd34ee568203847d1
Valentin Mihov <valentin.mihov@gmail.com>
2016-11-22 23:50:24 +0200

    Setup bootstrap and font-awesome dependencies

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule MelodicaInventory.PageController do
   use MelodicaInventory.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", trello_key: Application.get_env(:trello, :api_key)
   end
 end

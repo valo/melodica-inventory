0ecfbf38c1072ed3c7f817464a3fcc80ab8df96f
Valentin Mihov <valentin.mihov@gmail.com>
2017-02-20 20:20:55 +0200

    Use the correct config key for the DB settings

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :melodica_inventory, MelodicaInventory.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :melodica_inventory, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),

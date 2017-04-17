4e423c3028510af36578188d9aeec2d503b6aeed
Valentin Mihov <valentin.mihov@gmail.com>
2017-04-18 00:41:50 +0300

    Added some controller tests
    For the auth controller and the categories controller

---
+++ test/controllers/auth_controller_test.exs
@@ -0,0 +1,11 @@
+defmodule MelodicaInventory.AuthControllerTest do
+  use MelodicaInventory.Web.ConnCase, async: true
+
+  test "index renders inde.html", %{conn: conn} do
+    response = conn
+    |> get(login_path(conn, :index))
+
+    assert response.resp_body =~ "Login with Google"
+    refute response.resp_body =~ "Categories"
+  end
+end
---
+++ test/controllers/category_controller_test.exs
@@ -0,0 +1,70 @@
+defmodule MelodicaInventory.CategoryControllerTest do
+  use MelodicaInventory.Web.ConnCase, async: true
+
+  import MelodicaInventory.Factory
+
+  @default_opts [
+    store: :cookie,
+    key: "foobar",
+    encryption_salt: "encrypted cookie salt",
+    signing_salt: "signing salt",
+    log: false
+  ]
+  @secret String.duplicate("abcdef0123456789", 8)
+  @signing_opts Plug.Session.init(Keyword.put(@default_opts, :encrypt, false))
+
+  describe "when not authorized" do
+    test "index redirects to login", %{conn: conn} do
+      response = conn
+      |> get(category_path(conn, :index))
+
+      assert redirected_to(response) =~ login_path(conn, :index)
+    end
+  end
+
+  describe "when a user is authorized" do
+    setup do
+      current_user = insert(:user)
+
+      conn = build_conn()
+
+      conn = put_in(conn.secret_key_base, @secret)
+      |> Plug.Session.call(@signing_opts)
+      |> fetch_session
+      |> put_session(:current_user, current_user.id)
+
+      {:ok, conn: conn, current_user: current_user}
+    end
+
+    test "index renders the categories", %{conn: conn} do
+      response = conn
+      |> get(category_path(conn, :index))
+
+      assert response.resp_body =~ "Categories"
+      refute response.resp_body =~ "Events"
+    end
+  end
+
+  describe "when an admin user is authorized" do
+    setup do
+      current_user = insert(:user, admin: true)
+
+      conn = build_conn()
+
+      conn = put_in(conn.secret_key_base, @secret)
+      |> Plug.Session.call(@signing_opts)
+      |> fetch_session
+      |> put_session(:current_user, current_user.id)
+
+      {:ok, conn: conn, current_user: current_user}
+    end
+
+    test "index renders the categories", %{conn: conn} do
+      response = conn
+      |> get(category_path(conn, :index))
+
+      assert response.resp_body =~ "Categories"
+      assert response.resp_body =~ "Events"
+    end
+  end
+end

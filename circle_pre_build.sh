1fc382f2aff2c159158f87a597df1fe11d9f790c
Valentin Mihov <valentin.mihov@gmail.com>
2017-04-22 19:43:09 +0300

    Some formatting fixes for the pre build script

--- circle_pre_build.sh
+++ circle_pre_build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 # Ensure exit codes other than 0 fail the build
 set -e
 # Check for asdf
-if ! asdf | grep version; then  
-   git clone https://github.com/HashNuke/asdf.git ~/.asdf;
- fi
- # Add plugins for asdf
- asdf plugin-add erlang https://github.com/HashNuke/asdf-erlang.git
- asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
+if ! asdf | grep version; then
+  git clone https://github.com/HashNuke/asdf.git ~/.asdf;
+fi
+# Add plugins for asdf
+asdf plugin-add erlang https://github.com/HashNuke/asdf-erlang.git
+asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
 
 # Extract vars from elixir_buildpack.config
 . elixir_buildpack.config
@@ -22,4 +23,3 @@ yes | mix deps.get
 mix local.rebar
 # Exit successfully
 exit 0
-

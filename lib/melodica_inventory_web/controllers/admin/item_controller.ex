c518de4a77dd5633e6a3477d21bd69f981c7c58b
tspenov <tsvetozar.penov@airhelp.com>
2017-08-13 22:05:48 +0300

    Delete images for an item, add more images

--- lib/melodica_inventory_web/controllers/admin/item_controller.ex
+++ lib/melodica_inventory_web/controllers/admin/item_controller.ex
@@ -1,27 +1,37 @@
 defmodule MelodicaInventoryWeb.Admin.ItemController do
   use MelodicaInventoryWeb, :controller
-  alias MelodicaInventory.Goods.{Item, ItemDestroy}
+  alias MelodicaInventory.Goods.{Item, ItemDestroy, Image, Variation}
+
+  alias Ecto.{Multi, Changeset}
+
+  def delete_images(conn, %{"images" => images, "id" => id} = params) do
+    delete_images(images)
+    redirect(conn, to: item_path(conn, :show, id))
+  end
 
   def edit(conn, %{"id" => id}) do
-    changeset = Repo.get!(Item, id, preload: [:variation])
-    |> Repo.preload(:variation)
-    |> Item.changeset
+    item = Repo.get!(Item, id)
+    |> Repo.preload([:variation, :images])
 
-    render conn, "edit.html", changeset: changeset
+    changeset = item |> Item.changeset
+
+    render conn, "edit.html", item: item, changeset: changeset
   end
 
   def update(conn, %{"id" => id, "item" => item_params}) do
-    changeset = Repo.get!(Item, id)
-    |> Repo.preload(:variation)
-    |> Item.changeset(item_params)
+    item = Repo.get!(Item, id)
+    |> Repo.preload([:variation, :images])
 
-    case Repo.update(changeset) do
-      {:ok, item} ->
-        conn
-        |> put_flash(:info, "Item updated successfully.")
-        |> redirect(to: category_path(conn, :show, item.variation.category_id))
-      {:error, changeset} ->
-        render(conn, "edit.html", changeset: changeset)
+    case Repo.transaction(update_item(item, item_params)) do
+      {:ok, %{item: item}} ->
+        redirect(conn, to: item_path(conn, :show, item.id))
+      {:error, :item, failed_changeset, _changes_so_far} ->
+        render(conn, "edit.html", item: item, changeset: failed_changeset)
+      {:error, :image, error, changes_so_far} ->
+        changeset = changes_so_far[:item]
+        |> Item.changeset
+        |> Changeset.add_error(:image, "cannot be uploaded! #{ error }")
+        render(conn, "edit.html", item: item, changeset: %{changeset | action: :update})
     end
   end
 
@@ -40,6 +50,45 @@ defmodule MelodicaInventoryWeb.Admin.ItemController do
         conn
         |> put_flash(:error, "#{item.name} could not be deleted")
         |> redirect(to: category_path(conn, :show, item.variation.category_id))
+    end
+  end
+
+  defp update_item(item, item_params) do
+    Multi.new()
+    |> Multi.update(:item, Item.changeset(item, item_params))
+    |> Multi.run(:image, fn state -> upload_images(state, item_params["image"]) end)
+  end
+
+  defp delete_images(images) do
+    if delete_by_public_id(images) == length(images) do
+      Image |> where([i], i.public_id in ^images) |> Repo.delete_all
+    end
+  end
+
+  defp delete_by_public_id(images) do
+    Cloudex.delete(images)
+    |> Enum.reduce(0, fn({:ok, _}, acc) -> 1 + acc end)
+  end
+
+  defp upload_images(%{item: %Item{}}, nil), do: {:error, "You need to upload an image"}
+
+  defp upload_images(%{item: %Item{id: item_id}}, uploaded_files) do
+    Enum.map(uploaded_files, fn(%Plug.Upload{path: filename}) ->
+      case Cloudex.upload(filename) do
+        [error: error] ->
+           :error
+        [ok: %Cloudex.UploadedImage{public_id: public_id}] ->
+          {:ok, Repo.insert(%Image{public_id: public_id, item_id: item_id})}
+      end
+    end)
+    |> format_response
+  end
+
+  defp format_response(upload_images) do
+    if :error in upload_images do
+      {:error, "Cannot save image!"}
+    else
+      {:ok, true}
     end
   end
 end
--- lib/melodica_inventory_web/views/admin/item_view.ex
+++ lib/melodica_inventory_web/views/admin/item_view.ex
@@ -1,3 +1,16 @@
 defmodule MelodicaInventoryWeb.Admin.ItemView do
   use MelodicaInventoryWeb, :view
+
+  alias MelodicaInventory.Goods.Item
+  alias Cloudex.Url
+
+  def cover_url(%Item{images: []}) do
+    []
+  end
+
+  def cover_url(%Item{images: images}) do
+    Enum.map(images, fn(image)  ->
+      {Url.for(image.public_id, %{width: 400, height: 300, crop: "limit"}), image.public_id}
+    end)
+  end
 end

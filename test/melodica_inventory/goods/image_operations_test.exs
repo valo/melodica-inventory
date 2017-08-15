cd438d5e6d0e4ef88949096e848d1d11fb0d2499
tspenov <tsvetozar.penov@airhelp.com>
2017-08-15 17:28:59 +0300

    fix and add specs

--- test/melodica_inventory/goods/image_operations_test.exs
+++ test/melodica_inventory/goods/image_operations_test.exs
@@ -8,29 +8,38 @@ defmodule MelodicaInventory.Goods.ImageOperationsTest do
   alias MelodicaInventory.Goods.{Item, ImageOperations}
   import MelodicaInventory.Factory
 
-  test "upload images" do
+  test "#upload_images" do
     item = insert(:item)
 
     with_mock Cloudex, [], [upload: fn _ -> [ok: %Cloudex.UploadedImage{public_id: "12345"}] end] do
-      ImageOperations.upload_images(%{item: item}, [%Plug.Upload{path: "test.png"}])
+      ImageOperations.upload_images(item.id, [%Plug.Upload{path: "test.png"}])
     end
 
     updated_item = Repo.get(Item, item.id) |> Repo.preload([:attachments, :images])
     assert List.first(updated_item.images).public_id == "12345"
   end
 
-  test "deleting images" do
+  test "#delete_images" do
     image = insert(:image)
     item = Repo.get(Item, image.item_id)
     |> Repo.preload([:attachments, :images])
 
-    with_mock Cloudex, [], [delete: fn _ -> [{:ok,  %Cloudex.DeletedImage{public_id: image.public_id}}] end] do
+    with_mock Cloudex, [], [delete: fn _ -> {:ok,  %Cloudex.DeletedImage{public_id: image.public_id}} end] do
       ImageOperations.delete_images([image.public_id])
 
-      assert called Cloudex.delete([image.public_id])
+      assert called Cloudex.delete(image.public_id)
     end
 
     updated_item = Repo.get(Item, item.id) |> Repo.preload([:attachments, :images])
     assert updated_item.images == []
+  end
+
+  test "#delete_images_from_cloudex" do
+    image = insert(:image)
+    with_mock Cloudex, [], [delete: fn _ -> [{:ok,  %Cloudex.DeletedImage{public_id: image.public_id}}] end] do
+      ImageOperations.delete_images_from_cloudex([image])
+
+      assert called Cloudex.delete([image.public_id])
+    end
   end
 end
